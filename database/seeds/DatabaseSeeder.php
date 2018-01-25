6b1425252b3eda6704482ce92b8f8978852e97f9
Sergio Julio Garcia Urdiales <sjgu1@alu.ua.es>
2018-01-25 14:04:59 +0100

    Seeders mensaje convers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,13 +14,15 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(TweetsTableSeeder::class);
         $this->call(SeguidoresTableSeeder::class);
-        $this->call(UsersConversationsTableSeeder::class);
         $this->call(RTTableSeeder::class);
         $this->call(LikeTableSeeder::class);
         $this->call(RespuestaTableSeeder::class);
         $this->call(ListasTableSeeder::class);
         $this->call(CategoriasTableSeeder::class);
         $this->call(TweetCategoriasTableSeeder::class);
+        $this->call(UsersConversationsTableSeeder::class);
+        $this->call(MessageTableSeeder::class);
+
 
     }
 }
